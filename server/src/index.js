2d5d8d22a73bd5b7e1b9dc95f2f03aa5ae0a8fca
nikan <nikanemami7@gmail.com>
2024-06-23 06:46:13 -0400

    Update v.1.0
    Models, schema, routes and database connection implemented

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,19 @@
+// important app library imports
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose';
 
-
+// express creation
 const app = express();
 
-
+// express middleware
 app.use(express.json());
 app.use(cors());
 
+
+//MongoDB connection
+mongoose.connect("mongodb+srv://nikanemami7:bmLMty4S7rUBHCfD@cluster0.6z92jtk.mongodb.net/recipes?retryWrites=true&w=majority&appName=Cluster0")
+
+
+// actually calling and running server
 app.listen(3001, () => console.log("Server Started"));
---
+++ server/src/routes/Users.js
@@ -0,0 +1,8 @@
+// importing libraries to make the routes work
+//express for server
+import express from 'express';
+// jwt and bcrypt for password hashing
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
+// defining the actual router for it to work
