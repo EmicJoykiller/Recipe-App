3a421c4143683d788cb72951c6a56c791a4d55c2
nikan <nikanemami7@gmail.com>
2024-07-02 19:24:27 -0400

    Update create-recipe.js

--- client/src/pages/create-recipe.js
+++ client/src/pages/create-recipe.js
@@ -1,3 +1,59 @@
+import { useState } from 'react';
+
 export const CreateRecipe = () => {
-    return <div>CreateRecipe</div>
+    const [recipe, setRecipe] = useState({
+        name: "",
+        ingredients: [],
+        instructions: "",
+        imageUrl: "",
+        cookingTime: 0,
+        userOwner: 0,
+    });
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setRecipe({ ...recipe, [name]: value });
+    };
+
+    const handleIngredientChange = (event, idx) => {
+        const { value } = event.target;
+        const ingredients = recipe.ingredients;
+        ingredients[idx] = value;
+        setRecipe({ ...recipe, ingredients });
+    };
+
+    const addIngredient = () => {
+        setRecipe({ ...recipe, ingredients: [recipe.ingredients, ""] });
+    };
+
+    return (
+        <div className="create-recipe">
+            <h2> Create Recipe</h2>
+            <form>
+                <label htmlFor="name">Name</label>
+                <input type="text" id="name" name="name" onChange={handleChange} />
+
+                <label htmlFor="description">Description</label>
+                <textarea id="description" name="description" onChange={handleChange}></textarea>
+
+                <label htmlFor="ingredients">Ingredients</label>
+                {recipe.ingredients.map((ingredient, idx) => (
+                    <input key={idx} type='text' name='ingredient' value={ingredient} onChange={(event) => handleIngredientChange(event, idx)} />
+                ))}
+                <button onClick={addIngredient} type='button'>Add Ingredient</button>
+
+
+                <label htmlFor="instructions">Instructions</label>
+                <textarea id="instructions" name="instructions" onChange={handleChange}></textarea>
+
+                <label htmlFor="imageUrl">Image URL</label>
+                <input type="text" id="imageUrl" name="imageUrl" onChange={handleChange} />
+
+                <label htmlFor="cookingTime">Cooking Time (minutes)</label>
+                <input type="number" id="cookingTime" name="cookingTime" onChange={handleChange} />
+
+                <button type='submit'>Create Recipe</button>
+            </form>
+        </div>
+    )
 };
