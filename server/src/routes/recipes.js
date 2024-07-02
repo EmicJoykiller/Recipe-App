4d53e6b25109a7d90a2037d57980eeceedfc19a6
nikan <nikanemami7@gmail.com>
2024-07-02 18:34:31 -0400

    Update recipes.js
    now able to get and post new recipes on  the backend

--- server/src/routes/recipes.js
+++ server/src/routes/recipes.js
@@ -4,8 +4,27 @@ import { RecipeModel } from "../models/Recipes.js";
 
 const router = express.Router();
 
+//get all the recipes api
+router.get("/", async (req, res) => {
+    try {
+        const response = await RecipeModel.find({});
+        res.json(response);
+    } catch (err) {
+        res.json(err);
+    }
+});
 
+//create recipe api
+router.post("/", async (req, res) => {
+    const recipe = new RecipeModel(req.body);
 
+    try {
+        const response = await recipe.save();
+        res.json(response);
+    } catch (err) {
+        res.json(err);
+    }
+});
 
 
 export { router as recipesRouter };
