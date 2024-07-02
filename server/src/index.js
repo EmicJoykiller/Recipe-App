1bf8c64c0d16675842b2ef51218f50038f877daa
nikan <nikanemami7@gmail.com>
2024-07-02 09:54:59 -0400

    rollback

--- server/src/index.js
+++ server/src/index.js
@@ -4,7 +4,7 @@ import cors from 'cors'
 import mongoose from 'mongoose';
 //importing routers
 import { userRouter } from './routes/Users.js'
-import { recipesRouter } from './routes/recipes.js';
+
 
 
 // express creation
@@ -16,7 +16,7 @@ app.use(cors());
 
 //importing userRouter file for server to use and where to use
 app.use("/auth", userRouter);
-app.use("/recipes", recipesRouter);
+
 
 
 //MongoDB connection
