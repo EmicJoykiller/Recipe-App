08cdbd23259211bc569f35076f1b4fe39279085f
nikan <nikanemami7@gmail.com>
2024-07-02 18:49:25 -0400

    all backend including creation and displaying recipes implemented

--- server/src/models/Users.js
+++ server/src/models/Users.js
@@ -6,6 +6,7 @@ import mongoose from "mongoose";
 const UserSchema = new mongoose.Schema({
     username: { type: String, require: true, unique: true },
     password: { type: String, require: true },
+    savedRecipes: [{ type: mongoose.Schema.Types.ObjectId, ref: "recipes" }],
 });
 
 //actual model to be generated on the database based on the schema and export it to actually deploy on it
